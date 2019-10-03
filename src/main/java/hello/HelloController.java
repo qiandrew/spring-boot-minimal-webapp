4d0b4f6b856e02141e6b092e30fc1232e13b33eb
Andrew Qi <andrew99qi@gmail.com>
2019-10-03 14:49:33 -0700

    ?

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/")
     public String index(){
-        *String html = "<h1>Hello World!</h1>\n" +
+        String html = "<h1>Hello World!</h1>\n" +
         "<p>This web app is powered by \n" +
         "<a href='https://github.com/qiandrew/spring-boot-minimal-webapp'>this github repo</a></p>\n";
         return html;
